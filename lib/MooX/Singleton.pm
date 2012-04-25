f04a2b04d774d5be10475e14881e61b0d6d21e53
Alex J. G. Burzyński <ajgb@cpan.org>
2012-04-26 00:28:02 +0100

    release first version v1.121160

--- lib/MooX/Singleton.pm
+++ lib/MooX/Singleton.pm
@@ -1,8 +1,40 @@
 
 package MooX::Singleton;
-use strictures 1;
+# ABSTRACT: turn your Moo class into singleton
 
+use strictures 1;
 use Role::Tiny;
+
+=head1 SYNOPSIS
+
+    package MyApp;
+    use Moo;
+    with 'MooX::Singleton';
+
+    package main;
+
+    my $instance = MyApp->instance(@optional_init_args);
+    my $same = MyApp->instance;
+
+
+=head1 DESCRIPTION
+
+Role::Tiny role that provides L<"instance"> method turning your object into singleton.
+
+=method instance
+
+    my $singleton = MyApp->instance(@args1);
+    my $same = MyApp->instance;
+    # @args2 are ignored
+    my $above = MyApp->instance(@args2);
+
+Creates a new object initialized with arguments provided and then returns it.
+
+NOTE: Subsequent calls to C<instance> will return the singleton instance ignoring
+any arguments. This is different from L<MooseX::Singleton> which does not allow any
+arguments.
+
+=cut
 
 sub instance {
     my $class = shift;
